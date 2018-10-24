cb45e6174ca4350b6ff31132eed34eaca834d111
jp8874 <jp8874@studnet.uni-lj.si>
2018-10-24 08:50:40 +0200

    rents nit

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM openjdk:11-jre-slim
+
+RUN mkdir /app
+
+WORKDIR /app
+
+ADD ./api/target/rents-api-1.0.0-SNAPSHOT.jar /app
+
+EXPOSE 8081
+
+CMD java -jar rents-api-1.0.0-SNAPSHOT.jar
---
+++ api/src/main/java/si/fri/rso/teamlj/rents/api/v1/RentsApplication.java
@@ -0,0 +1,8 @@
+package si.fri.rso.teamlj.rents.api.v1;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/v1")
+public class RentsApplication extends Application {
+}
